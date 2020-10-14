ee015a9341a773aa3cf177a869aed37e94415548
Austin Gordon <67164152+austincgordon@users.noreply.github.com>
2020-10-13 20:16:43 -0500

    Corrections
    Fixed the chart.js issues, and updated the CSS/js.

--- js/app.js
+++ js/app.js
@@ -1,128 +1,151 @@
 const alertBanner = document.getElementById('alert');
 
 // Creates the HTML for the banner
-alertBanner.innerHTML = 
-`
+alertBanner.innerHTML = `
 <div class="alert-banner">
 <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
 to complete</p>
 <p class="alert-banner-close">x</p>
 </div>
-`
+`;
 
 // closes the banner when 'x' is clicked
-alertBanner.addEventListener('click', e => {
-    const element = e.target;
-    if (element.classList.contains("alert-banner-close")) {
-        alertBanner.style.display = "none"
-    }
+alertBanner.addEventListener('click', (e) => {
+	const element = e.target;
+	if (element.classList.contains('alert-banner-close')) {
+		alertBanner.style.display = 'none';
+	}
 });
 
 // Traffic Chart
 let trafficCanvas = document.getElementById('traffic-chart').getContext('2d');
-let trafficChart = new Chart(trafficCanvas, {
-    type: 'line',
-    data: trafficData,
-    options: trafficOptions
-});
 
 let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-        backgroundColor: 'rgba(116, 119, 191, .3)',
-        borderWidth: 1,
-    }]
+	labels: [
+		'16-22',
+		'23-29',
+		'30-5',
+		'6-12',
+		'13-19',
+		'20-26',
+		'27-3',
+		'4-10',
+		'11-17',
+		'18-24',
+		'25-31',
+	],
+	datasets: [
+		{
+			data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+			backgroundColor: 'rgba(116, 119, 191, .3)',
+			borderWidth: 1,
+		},
+	],
 };
 
 let trafficOptions = {
-    aspectRatio: 2.5,
-    animation: {
-        duration: 0
-    },
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
-    },
-    legend: {
-        display: true
-    }
+	aspectRatio: 2.5,
+	animation: {
+		duration: 0,
+	},
+	scales: {
+		yAxes: [
+			{
+				ticks: {
+					beginAtZero: true,
+				},
+			},
+		],
+	},
+	legend: {
+		display: false,
+	},
 };
 
-// Mobile Users Chart
-let mobileCanvas = document.getElementById("mobile-chart").getContext('2d');
-let mobileChart = new Chart(mobileCanvas, {
-    type: 'doughnut',
-    data: mobileData,
-    options: mobileOptions
+let trafficChart = new Chart(trafficCanvas, {
+	type: 'line',
+	data: trafficData,
+	options: trafficOptions,
 });
 
-​let​ mobileData = {
-     labels: ["Desktop", "Tablet", "Phones"],
-     datasets: [{
-         label: '# of Users',
-         data: [2000, 550, 500],
-         borderWidth: 0,
-         backgroundColor: [
-        ​    '#7477BF'​,
-            '#78CF82',
-             '#51B6C8'
-         ]
-    }]
- };
+// Mobile Users Chart
+let mobileCanvas = document.getElementById('mobile-chart').getContext('2d');
 
+let mobileData = {
+	labels: ['Desktop', 'Tablet', 'Phones'],
+	datasets: [
+		{
+			label: '# of Users',
+			data: [2000, 550, 500],
+			borderWidth: 0,
+			backgroundColor: ['#7477BF', '#78CF82', '#51B6C8'],
+		},
+	],
+};
 let mobileOptions = {
-    legend: {
-        position: 'right',
-        labels: {
-            boxWidth: ​20​,
-            fontStyle: 'bold'
-        }
-    }
-}
+	legend: {
+		position: 'right',
+		labels: {
+			boxWidth: 20,
+			fontStyle: 'bold',
+		},
+	},
+};
 
+let mobileChart = new Chart(mobileCanvas, {
+	type: 'doughnut',
+	data: mobileData,
+	options: mobileOptions,
+});
 
 // Daily Taffic Chart
-​let dailyCanvas = ​document​.getElementById(​"daily-chart"​).getContext('2d');
-​let​ dailyChart = ​new​ Chart(dailyCanvas, { type: ​'bar'​,
-data: dailyData,
-options: dailyOptions
+let dailyCanvas = document.getElementById('daily-chart').getContext('2d');
+
+let dailyData = {
+	labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+	datasets: [
+		{
+			label: '# of Hits',
+			data: [75, 115, 175, 125, 225, 200, 100],
+			backgroundColor: '#7477BF',
+			borderWidth: 1,
+		},
+	],
+};
+
+let dailyOptions = {
+	scales: {
+		yAxes: [
+			{
+				ticks: {
+					beginAtZero: true,
+				},
+			},
+		],
+	},
+	legend: {
+		display: false,
+	},
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+	type: 'bar',
+	data: dailyData,
+	options: dailyOptions,
 });
 
-​let dailyData = {
-    labels: [​"S"​, ​"M"​, ​"T"​, ​"W"​, ​"T"​, ​"F"​, ​"S"​], datasets: [{
-    label: ​'# of Hits'​,
-    data: [​75​, ​115​, ​175​, ​125​, ​225​, ​200​, ​100​], backgroundColor: ​'#7477BF'​,
-    borderWidth: ​1
-     }]
- };
-    
-let dailyOptions = {
-    scales: {
-        yAxes: [{
-          ticks: {
-    beginAtZero:​true }
-    }] },
-    legend : {
-    display: ​false }
-    }
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
 
-const user = document.getElementById("userField");
-const message = document.getElementById("messageField");
-const send = document.getElementById("send");
-
-send.addEventListener(​'click'​, () => {
-
-    ​if​ (user.value === ​""​ && message.value === ​""​) {
-    alert(​"Please fill out user and message fields before sending"​);
-    } ​else​ ​if​ (user.value === ​""​ ) {
-    alert(​"Please fill out user field before sending"​);
-    } ​else​ ​if​ (message.value === ​""​ ) {
-    alert(​"Please fill out message field before sending"​);
-    } ​else​ {
-    alert(`Message successfully sent to: ${user.value}`);
-}
+send.addEventListener('click', () => {
+	if (user.value === '' && message.value === '') {
+		alert('Please fill out user and message fields before sending');
+	} else if (user.value === '') {
+		alert('Please fill out user field before sending');
+	} else if (message.value === '') {
+		alert('Please fill out message field before sending');
+	} else {
+		alert(`Message successfully sent to: ${user.value}`);
+	}
 });
